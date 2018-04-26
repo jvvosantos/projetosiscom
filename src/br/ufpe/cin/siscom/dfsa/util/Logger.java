7119fcfca9dcb680dd6644e471baa290710f878c
=Joao Santos <jvos@cin.ufpe.br>
2018-04-26 20:34:06 -0300

    Adição do pacote util: Logger e Random Number Generator

---
+++ src/br/ufpe/cin/siscom/dfsa/util/Logger.java
@@ -0,0 +1,55 @@
+package br.ufpe.cin.siscom.dfsa.util;
+
+public class Logger {
+	
+	/**
+	 * Logs a single object (same as calling method from {@link System}
+	 * @author jvos
+	 * */
+	public static void log(Object object) {
+		System.out.println(object);
+	}
+	
+	/**
+	 * Logs an undetermined number of objects as parameter
+	 * @author jvos
+	 * */
+	public static void log(Object ... objects) {
+		int i = 0;
+		for (Object o : objects) {
+			System.out.print(o);
+			if (i < objects.length-1) {
+				System.out.print(", ");
+			}
+			i++;
+		}
+		System.out.println();
+	}
+	
+	/**
+	 * Logs a determined milissecond timestamp to a formated hour, minute, second, milissecond String
+	 * @author jvos
+	 * */
+	public static void logFinishTime(String log, long endTime) {
+		
+		System.out.print(log+" finished running in: ");
+		long hours = ((endTime / (1000 * 60 * 60)) % 24);
+		long minutes = ((endTime / (1000 * 60)) % 60);
+		long seconds = ((endTime / 1000) % 60);
+		
+		if (hours > 0) {			
+			System.out.print(hours+" h ");
+		}
+		if (minutes > 0) {			
+			System.out.print(minutes+" min ");
+		}
+		if (seconds > 0) {			
+			System.out.print(seconds+" s ");
+		}
+		else {
+			System.out.print(endTime+" ms");
+		}
+		System.out.println();
+	}
+
+}
---
+++ src/br/ufpe/cin/siscom/dfsa/util/RNG.java
@@ -0,0 +1,13 @@
+package br.ufpe.cin.siscom.dfsa.util;
+
+import java.util.Random;
+
+public class RNG {
+
+	private static Random rngesus = new Random();
+
+	public static int generateRandomInteger(int max) {
+		return rngesus.nextInt(max+1);
+	}
+
+}
